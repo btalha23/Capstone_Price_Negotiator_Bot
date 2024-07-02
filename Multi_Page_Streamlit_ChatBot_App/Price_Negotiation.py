1c2f07c2e346fbeb36141e7d774da33f9f1c6037
Batool Talha <batool.talha@novelda.com>
2024-07-02 05:26:55 +0200

    ¤ Multi_Page_Streamlit_ChatBot_App/Price_Negotiation.py - updates to the prompts for the chatbot; eligility of the loyalty customer seems to work now

--- Multi_Page_Streamlit_ChatBot_App/Price_Negotiation.py
+++ Multi_Page_Streamlit_ChatBot_App/Price_Negotiation.py
@@ -7,9 +7,9 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv, find_dotenv
 
-def init_database(user: str, password: str, host: str, port: str, database: str) -> SQLDatabase:
-  db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
-  return SQLDatabase.from_uri(db_uri)
+loyalty_customer_eligibity = {
+    "LOYALTY_CUSTOMER_NUM_PURCHASES": 10,
+    "LOYALTY_CUSTOMER_NUM_PURCHASE_DAYS": 10}
 
 st.title("Price Negotiator Chatbot")
 
@@ -17,8 +17,11 @@ load_dotenv(find_dotenv())
 
 def get_sql_chain(db):
     template = """
-        You are a sales representative at a company. You are interacting with a user who is asking you 
-        to reduce the price of products in the shopping cart.
+        You are a sales representative at a company. You are interacting with customers to offer them special
+        discounts. These discounts are subject to if the customer qualifies as a loyalty customer. 
+        To be a loyalty customer there should be a purchase record associated with the customer 
+        for a minumun of 10 unique dates. If there are more than one purchase on a day, count that day only
+        once.
 
         Based on the table schema below, write a SQL query that would answer the user's question. 
         Take the conversation history into account.
@@ -31,6 +34,12 @@ def get_sql_chain(db):
         not even backticks.
 
         For example:
+        Question: Check for being a loyalty customer
+        SQL Query: SELECT customer_id, COUNT(DISTINCT DATE(interaction_date)) AS purchase_days
+                   FROM CustomerInteractions
+                   WHERE interaction_type = 'purchase'
+                   GROUP BY customer_id
+                   HAVING purchase_days >= 10;
         Question: What is the average price of the saucepans?
         SQL Query: SELECT AVG(product_price) AS average_price FROM product WHERE product_name = 'Saucepan';
         Question: Name 10 products
@@ -49,11 +58,11 @@ def get_sql_chain(db):
     
     def get_schema(_):
         return db.get_table_info()
-    
+        
     return (
         RunnablePassthrough.assign(schema=get_schema)
         | prompt
-        | llm
+        | llm.bind(stop=["\nSQL Result:"])
         | StrOutputParser()
     )
 
@@ -61,117 +70,22 @@ def get_response(user_query: str, db: SQLDatabase, chat_history: list):
     sql_chain = get_sql_chain(db)
     
     template = """
-        You are a sales representative at a company. You are interacting with a user who is asking you 
-        questions about the products in their shopping cart. Follow these guidelines during the interaction:
-
-        1. Do not offer a discount unless the customer explicitly asks for a price reduction.
-           If the customer is asking for the price or other product details without mentioning a discount, 
-           provide an exact answer without offering a discount.
-        2. If the customer asks for a discount, do not disclose the maximum possible discount based 
-           on the product price margin percentage in the first interaction.
-           Offer a small discount incrementally.
-        3. If there are more multiple prices of the same product in the product catelogue, 
-           always use the maximum price in customer engagement.
-        4. Engage in a bargaining conversation if the customer asks for further reductions.
-           Increase the discount in small steps and always wait for the customer's response before 
-           making further offers.
-        5. If the customer is satisfied with a price and does not ask for more discounts, 
-           do not offer additional discounts. Lock the price that is acceptable for the customer.
-        6. Always respond with understanding and empathy.
-           Avoid giving detailed explanations for the discounts and focus on maintaining a friendly 
-           and helpful tone.
-
-        Based on the table schema below, question, sql query, sql response, and example response
-        write a natural language response.
+        You are a sales representative at a company. You are interacting with customers to offer them special
+        discounts. These discounts are subject to if the customer qualifies as a loyalty customer. 
+        To be a loyalty customer there should be a purchase record associated with the customer 
+        for a minumun of 10 unique dates. If there are more than one purchase on a day, count that day only
+        once. If the sql response is empty, then get the actual number of purchase days and inform 
+        the customer that the customer is not eligible for the discount. 
         
+        Based on the table schema below, question, sql query, and sql response, write a natural language response.
+        The natural language response should not have any mention of SQL response and SQL query.
+
         <SCHEMA>{schema}</SCHEMA>
-
-        Examples of past interactions:
-        
+    
         Conversation History: {chat_history}
-
-        Example 1:
-        User Question: What is the price of the Premium Frying Pan?
-        SQL Query: SELECT MAX(product_price) 
-                   FROM product 
-                   WHERE product_name = 'frying pan';
-        SQL Response: 60.00
-        Response: The price for the Premium Frying Pan is $60.00.
-        
-        Example 2:
-        User Question: Can you give me a better price on the Deluxe Saucepan?
-        SQL Query: SELECT MAX(product_price) 
-                   FROM product 
-                   WHERE product_name = 'saucepan';
-        SQL Response: 50.00
-        Response: The price for the Deluxe Saucepan is $50.00. I can offer you a small discount of $1.00. 
-                   How does that sound?
-
-        User Question: Can you do better than that?
-        SQL Query: SELECT MAX(product_price * product_margin_percent / 100) * 0.25 AS medium_discount FROM product WHERE product_name = 'saucepan';
-        SQL Response: 2.50
-        Response: I understand you want a better deal. I can increase the discount to $2.50. 
-                   Does this work for you?
-
-        User Question: I am interested to ask for an even lower price?
-        SQL Query: SELECT MAX(product_price * product_margin_percent / 100) * 0.5 AS fifty_percent_discount FROM product WHERE product_name = 'saucepan';
-        SQL Response: 5.0
-        Response: It is good to see your interest in the product. Based on your interest I can take off $5.0 
-                  from the original price. Is it okay?
-
-        User Question: I was hoping for a bit more.
-        SQL Query: SELECT MAX(product_price * product_margin_percent / 100) * 0.75 AS large_discount 
-                   FROM product 
-                   WHERE product_name = 'saucepan';
-        SQL Response: 7.50
-        Response: You're driving a hard bargain! How about $7.50 off? This is my best offer.
-
-        Example 3:
-        User Question: I'm looking to get a discount on the Premium Frying Pan.
-        SQL Query: SELECT MAX(product_price) 
-                   FROM product 
-                   WHERE product_name = 'frying pan';
-        SQL Response: 60.00
-        Response: The price for the Premium Frying Pan is $60.00. I can offer you a $1.20 discount. 
-                   What do you think?
-
-        User Question: Can you lower the price a bit more?
-        SQL Query: SELECT MAX(product_price * product_margin_percent / 100) * 0.25 AS medium_discount 
-                   FROM product 
-                   WHERE product_name = 'frying pan';
-        SQL Response: 3.00
-        Response: I can stretch it a bit further and offer you $3.00 off. Does that sound good?
-
-        User Question: I am interested to ask for an even lower price?
-        SQL Query: SELECT MAX(product_price * product_margin_percent / 100) * 0.5 AS fifty_percent_discount FROM product WHERE product_name = 'saucepan';
-        SQL Response: 6.75
-        Response: It is good to see your interest in the product. Based on your interest I can take off $6.75 
-                  from the original price. Is it okay?
-
-        User Question: I'm still looking for a better deal.
-        SQL Query: SELECT MAX(product_price * product_margin_percent / 100) * 0.75 AS large_discount 
-                   FROM product 
-                   WHERE product_name = 'frying pan';
-        SQL Response: 8,25
-        Response: Alright, I can go up to $8.25 off. This is the final discount I can offer.
-
-        Example 4:
-        User Question: What is the price after the discount.
-        SQL Query: SELECT
-                        MAX(product_price), 
-                        MAX(product_price * product_margin_percent / 100) * 0.08 AS large_discount,
-                        MAX(product_price - (product_price * product_margin_percent / 100) * 0.08) AS final_price, 
-                   FROM product 
-                   WHERE product_name = 'frying pan';
-        SQL Response: 55.20
-        Response: The price after the discount is $55.20.
-
-
-        Your turn:
         SQL Query: <SQL>{query}</SQL>
         User Question: {question}
         SQL Response: {response}
-        Response: 
         """
     
     prompt = ChatPromptTemplate.from_template(template)
@@ -195,17 +109,10 @@ def get_response(user_query: str, db: SQLDatabase, chat_history: list):
 
 if "chat_history" not in st.session_state:
   st.session_state.chat_history = [
-    AIMessage(content="Hello, I am a price negotiator bot. Lets start bargaining")
+    AIMessage(content="""Hello, I am a price negotiator bot. 
+              Loyalty customers are eligible for special offers. 
+              Lets start evaluating your eligibilty.""")
   ]
-
-db = init_database(
-  user="root",
-  password="root",
-  host="localhost",
-  port="3306",
-  database="price_negotiation"
-)
-st.session_state.db = db
 
 for message in st.session_state.chat_history:
     if isinstance(message, AIMessage):
